5f1e8a4ef96c103f9bfa1e6093fe379edeab732b
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-25 14:13:29 -0500

    split webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'development',
   entry: {
     bundle: path.resolve(__dirname, 'src/js/index.js')
   },
@@ -12,17 +10,6 @@ module.exports = {
     filename: '[name][contenthash].js',
     clean: true,
     assetModuleFilename: '[name][ext]',
-  },
-  devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist')
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -60,7 +47,5 @@ module.exports = {
       filename: 'index.html',
       template: 'src/template.html',
     }),
-    // uncomment for space analyzer
-    // new BundleAnalyzerPlugin(),
   ],
 };
---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    // uncomment for space analyzer
+    // new BundleAnalyzerPlugin(),
+  ],
+});
